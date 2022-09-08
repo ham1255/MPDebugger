8124856036abaee45f14b8c1878f87ed99f8a4f5
mohammed jasem alaajel <xrambad@glomc.net>
2022-09-08 18:02:47 +0400

    add project files
    can't be compiled yet.

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    `java-library`
+    id("io.papermc.paperweight.userdev") version "1.3.8"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2" // Generates plugin.yml
+}
+
+group = "net.glomc.multipaper.plugins"
+version = "1.0-SNAPSHOT"
+description = "plugin that extends Multipaper debugging process."
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+dependencies {
+    paperweightDevBundle("puregero.multipaper", "1.19.2-R0.1-SNAPSHOT")
+}
+
+tasks {
+    // Configure reobfJar to run when invoking the build task
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+
+        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
+        // See https://openjdk.java.net/jeps/247 for more information.
+        options.release.set(17)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+
+    /*
+    reobfJar {
+      // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
+      // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
+      outputJar.set(layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar"))
+    }
+     */
+}
+
+// Configure plugin.yml generation
+bukkit {
+    load = BukkitPluginDescription.PluginLoadOrder.STARTUP
+    main = "net.glomc.multipaper.plugins.mpdebugger.MPDebugger"
+    apiVersion = "1.19"
+    authors = listOf("Ham1255")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+rootProject.name = "MPDebugger"
+
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenLocal()
+        mavenCentral()
+        // limework maven server no longer exists.
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
